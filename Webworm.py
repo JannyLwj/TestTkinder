f886b0b0d5f16a00de855c362e486acd873446e6
Janny <187231153@qq.com>
2016-09-28 14:42:05 +0800

    Version2

--- Webworm.py
+++ Webworm.py
@@ -3,6 +3,8 @@ import urllib
 import urllib.request
 import urllib.parse
 import http.cookiejar
+import re
+
 
 def open_url():
     values = {}
@@ -61,6 +63,61 @@ def test_readfromcookie():
     response = opener.open(req)
     print(response.read())
 
+def re_model():
+    # 将正则表达式编译成Pattern对象，注意hello前面的r的意思是“原生字符串”
+    pattern = re.compile(r'hello')
+
+    # 使用re.match匹配文本，获得匹配结果，无法匹配时将返回None
+    result1 = re.search(pattern, 'yjhello')
+    result2 = re.match(pattern, 'helloo CQC!')
+    result3 = re.match(pattern, 'helo CQC!')
+    result4 = re.match(pattern, 'hello CQC!')
+
+    # 如果1匹配成功
+    if result1:
+        # 使用Match获得分组信息
+        print(result1.group())
+    else:
+        print('1匹配失败！')
+
+    # 如果2匹配成功
+    if result2:
+        # 使用Match获得分组信息
+        print(result2.group())
+    else:
+        print('2匹配失败！')
+
+        # 如果3匹配成功
+    if result3:
+        # 使用Match获得分组信息
+        print(result3.group())
+    else:
+        print('3匹配失败！')
+
+        # 如果4匹配成功
+    if result4:
+        # 使用Match获得分组信息
+        print(result4.group())
+    else:
+        print('4匹配失败！')
+
+def test_findall():
+    pattern=re.compile(r'\d+')
+    print(re.split(pattern,'one11two2three3four4'))
+    print(re.findall(pattern, 'one11two2three3four4'))
+    for m in re.finditer(pattern,'one11two2three3four4'):
+        print(m.group())
+
+    pattern1 = re.compile(r'(\w+) (\w+)')
+    s = 'i say, hello world!'
+    print(re.sub(pattern1, r'\2 \1', s))
+
+    def func(m):
+        return m.group(1).title() + ' ' + m.group(2).title()
+    print(re.sub(pattern1, func, s))
+
+
+
 if __name__=='__main__':
-    test_readfromcookie()
+    test_findall()
 
